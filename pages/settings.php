bc2cc02b2c51218e6f9819903c29e25e5b6cfb3e
takitahmid20 <takitahmid20@gmail.com>
2026-06-14 23:06:43 +0600

    feat: logout action and require_login guard on app pages
    Guard all 9 app pages, add logout handler, show logged-in user name and a
    logout button in the navbar.

--- pages/settings.php
+++ pages/settings.php
@@ -1,3 +1,7 @@
+<?php
+require_once __DIR__ . '/../includes/bootstrap.php';
+require_login();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- partials/navbar.php
+++ partials/navbar.php
@@ -56,6 +56,7 @@
 			</div>
 		</div>
 
+		<?php $navUser = function_exists('current_user') ? current_user() : null; ?>
 		<button
 			class="navbar-user"
 			type="button"
@@ -64,12 +65,16 @@
 			onclick="window.location.href='./settings.php'"
 		>
 			<span class="navbar-user-avatar" data-user-avatar>
-				<img src="https://api.dicebear.com/9.x/adventurer-neutral/svg?seed=Sophie" alt="User avatar" />
+				<img src="https://api.dicebear.com/9.x/adventurer-neutral/svg?seed=<?= e(urlencode($navUser['name'] ?? 'User')) ?>" alt="User avatar" />
 			</span>
 			<span class="navbar-user-meta">
-				<span class="navbar-user-name" data-user-name></span>
+				<span class="navbar-user-name" data-user-name><?= e($navUser['name'] ?? '') ?></span>
 			</span>
 		</button>
+
+		<a class="btn btn-outline btn-icon navbar-logout" href="../actions/logout.php" aria-label="Log out" title="Log out">
+			<i data-lucide="log-out" aria-hidden="true"></i>
+		</a>
 
 	</div>
 </header>
